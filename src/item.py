a6063ac82a01267ed5c099268698219a7ae85a67
vitali <sh015@mail.ru>
2023-05-22 21:13:34 +0300

    1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import json
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +10,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int):
         """
         Создание экземпляра класса item.
 
@@ -13,7 +18,26 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.general_summ = self.price * self.quantity
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            print('Длина товара превышает 10 символов')
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __str__(self):
+        return f"{self.__name}"
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +45,41 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+
+        return self.price * self.quantity
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+        return int(self.quantity) + int(other.quantity)
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+    @classmethod
+    def instantiate_from_csv(cls):
+        path = os.path.join(os.path.dirname(__file__), "items.csv")
+        with open(path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all.clear()
+            for row in reader:
+                print(row)
+                cls.all.append(cls(row['name'], int(row['price']), int(row['quantity'])))
+
+        return len(cls.all)
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, count_sim):
+        if not isinstance(count_sim, int) or count_sim <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+        self.__number_of_sim = count_sim
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,30 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+from src.phone import Phone
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+phone1 = Phone("iPhone 14", 120000, 5, 2)
+Item.pay_rate = 0.8
+
+def test_calculate_total_price():
+    assert item1.price * item1.quantity == 200000
+    assert item2.price * item2.quantity == 100000
+
+def test_apply_discount():
+    assert  item1.price * Item.pay_rate == 8000
+    assert  item2.price * Item.pay_rate == 16000
+
+def test_repr():
+  #  assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert int(repr(item1.quantity)) + int(repr(phone1.quantity)) == 25
+    assert phone1.quantity + phone1.quantity == 10
+
+
+def test_str():
+    assert str(item1) == 'Смартфон'
+
+def test_add():
+    assert item1 + phone1 == 25
+
