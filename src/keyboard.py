de057372c8f41951a5c235a1f7212578a7ae8494
vitali <sh015@mail.ru>
2023-06-07 22:40:03 +0300

    1

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,23 +1,31 @@
 from src.item import Item
 
+class LanguageMixin:
+    __language = 'EN'
 
-class MixinLog:
-    ENG = "EN"
-
-    def __init__(self):
-        self.__language = "EN"
-
-    def change_lang(self):
-        if self.__language == "EN":
-            self.__language = "RU"
-        elif self.__language == "RU":
-            self.__language = "EN"
-        return self
+    def __init__(self, __language):
+        self.__language = __language
 
     @property
     def language(self):
         return self.__language
 
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+            return self
+        else:
+            self.__language == 'EN'
+            return self
 
-class KeyBoard(Item, MixinLog):
-    pass
+
+class Keyboard(Item, LanguageMixin):
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"Keyboard('{self.name}', {self.price}, {self.quantity})"
--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,18 +1,26 @@
 import pytest
-from src.keyboard import KeyBoard
+from src.keyboard import LanguageMixin, Keyboard
 
+def test_init():
+    obj = LanguageMixin('RU')
+    assert obj.language == 'RU'
 
-@pytest.fixture
-def keyboard():
-    return KeyBoard('Dark Project KD87A', 9600, 5)
+def test_change_lang():
+    obj = LanguageMixin('EN')
+    obj.change_lang()
+    assert obj.language == 'RU'
+    obj.change_lang()
 
+def test_keyboard_creation():
+    keyboard = Keyboard("Logitech K380", 2999.0, 10)
+    assert keyboard.name == "Logitech K380"
+    assert keyboard.price == 2999.0
+    assert keyboard.quantity == 10
 
-def test_change_lang(keyboard):
-    assert str(keyboard) == "Dark Project KD87A"
-    assert str(keyboard.language) == "EN"
-    keyboard.change_lang()
-    assert str(keyboard.language) == "RU"
-    keyboard.change_lang().change_lang()
-    assert str(keyboard.language) == "RU"
-    with pytest.raises(AttributeError):
-        keyboard.language = 'FR'
+def test_keyboard_str():
+    keyboard = Keyboard("Logitech K380", 2999.0, 10)
+    assert str(keyboard) == "Logitech K380"
+
+def test_keyboard_repr():
+    keyboard = Keyboard("Logitech K380", 2999.0, 10)
+    assert repr(keyboard) == "Keyboard('Logitech K380', 2999.0, 10)"
